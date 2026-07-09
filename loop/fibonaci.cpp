852f4e18442e800145ab9aea904f1ad9802ced18
Rahul Chand <chandrahuls2212@gmail.com>
2026-07-09 08:01:19 +0530

    we can print whole series also

--- loop/fibonaci.cpp
+++ loop/fibonaci.cpp
@@ -5,10 +5,12 @@ int main(){
     int ans;
     int n =5;
     int last = 1,slast=0;
+    cout<<slast<<"\n"<<last<<"\n";
     for(int i=2;i<=n;i++){
-         ans = slast+last;
+        ans = slast+last;
        slast = last;
        last=ans;    //last he n fibonachi hota hai
+       cout<<ans<<endl;
     }
     cout<<n<<" fibonacie number : "<<ans;
     return 0;
