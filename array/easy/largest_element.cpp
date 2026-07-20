fae9de128581887530f4cf5ea25a98b9e728bdd8
Rahul Chand <chandrahuls2212@gmail.com>
2026-07-20 09:21:59 +0530

    largest element

---
+++ array/easy/largest_element.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+        int maximum = INT_MIN;
+        for(int i=0;i<nums.size();i++){
+              maximum = max(maximum,nums[i]);
+        }
+      
+
+        return maximum;
+    }
+};
