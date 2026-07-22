4cbeb796624c6d54b5f795b563a156a12302cd61
Rahul Chand <chandrahuls2212@gmail.com>
2026-07-22 12:47:22 +0530

    brute froce approch using set data structure

--- array/easy/remove_duplicate.cpp
+++ array/easy/remove_duplicate.cpp
@@ -3,39 +3,50 @@ using namespace std;
 
 class Solution {
 public:
-    void removeDuplicates(vector<int>& nums) {
-        //brute force
+    int removeDuplicates(vector<int>& nums) {
+        //brute force tc => n^3;
+        // int n = nums.size();
+        // for(int i=0;i<n;i++){
+        //     int count = 0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[i]==nums[j]){
+        //             count++;
+        //             if(count>=2 ){
+        //                 for(int k = j;k<n-1;k++){
+        //                     //shifting
+        //                     nums[k]=nums[k+1];
+        //                 }
+        //                 nums.pop_back();
+        //                 n=nums.size();
+        //             }
+        //         }
+
+        //     }
+
+        // }
+        // return n;
+
+ /*=====================================================================*/
+        // brute froce using set tc => nlogn + n
         int n = nums.size();
+        set<int>st;
         for(int i=0;i<n;i++){
-            int count = 0;
-            for(int j=0;j<n;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                    if(count>=2 ){
-                        for(int k = j;k<n-1;k++){
-                            //shifting
-                            nums[k]=nums[k+1];
-                        }
-                        nums.pop_back();
-                        n=nums.size();
-                    }
-                }
-
-            }
-
+            st.insert(nums[i]);
         }
 
-        for(int i=0;i<n;i++){
-            cout<<nums[i]<<" ";
+        int index = 0;
+        for(auto i : st){
+            nums[index] = i;
+            index++;
         }
-
-
+        
+        return index;
     }
 };
 
 int main(){
     vector<int>nums ={1,2,2,2,3,3,4,4,4,5};
     Solution s1;
-    s1.removeDuplicates(nums);
+   cout<<s1.removeDuplicates(nums);
     return 0;
 }
