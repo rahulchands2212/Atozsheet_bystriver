145b92b1487570f761aaffb90e58edf9ba3acb74
Rahul Chand <chandrahuls2212@gmail.com>
2026-07-30 20:32:40 +0530

    better approch by sir

--- array/medium/twoSum.cpp
+++ array/medium/twoSum.cpp
@@ -25,53 +25,66 @@ public:
         // }
         // return ans;
         /*--------------------------------- better approch think by me ------------------------------------------------*/
-        vector<int> v2 = nums;
-        sort(v2.begin(), v2.end());
-        int low = 0;
-        int high = nums.size() - 1;
-        int sum = 0;
-        int num1, num2;
-        // sum
-        while (low < high)
-        {
-            sum = v2[low] + v2[high];
+        // vector<int> v2 = nums;
+        // sort(v2.begin(), v2.end());
+        // int low = 0;
+        // int high = nums.size() - 1;
+        // int sum = 0;
+        // int num1, num2;
+        // // sum
+        // while (low < high)
+        // {
+        //     sum = v2[low] + v2[high];
 
-            if (sum == target)
-            {
-                num1 = v2[low];
-                num2 = v2[high];
-                break;
-            }
-            else if (sum < target)
-            {
-                low++;
-            }
-            else
-            {
-                high--;
-            }
-        }
-        // search
-        int n = nums.size();
-        int first = -1;
-        int second = -1;
-        vector<int> ans;
+        //     if (sum == target)
+        //     {
+        //         num1 = v2[low];
+        //         num2 = v2[high];
+        //         break;
+        //     }
+        //     else if (sum < target)
+        //     {
+        //         low++;
+        //     }
+        //     else
+        //     {
+        //         high--;
+        //     }
+        // }
+        // // search
+        // int n = nums.size();
+        // int first = -1;
+        // int second = -1;
+        // vector<int> ans;
 
-        for (int i = 0; i < n; i++)
-        {
-            if (nums[i] == num1 && first == -1)
-            {
-                first = i;
-            }
-            else if (nums[i] == num2)
-            {
-                second = i;
-            }
-        }
-            ans.push_back(first);
-             ans.push_back(second);
+        // for (int i = 0; i < n; i++)
+        // {
+        //     if (nums[i] == num1 && first == -1)
+        //     {
+        //         first = i;
+        //     }
+        //     else if (nums[i] == num2)
+        //     {
+        //         second = i;
+        //     }
+        // }
+        //     ans.push_back(first);
+        //      ans.push_back(second);
            
-        return ans;
+        // return ans;
+
+        /*----------------------------------------- better approch by sir -------------- using hash --*/
+        map<int,int>mpp;
+        int n = nums.size();
+        for(int i =0;i<n;i++){
+            int current = nums[i];
+            int rem = target - current;
+            if(mpp.find(rem)!=mpp.end()){
+                return {mpp[rem],i};
+            }
+            mpp[current] = i;
+        }
+        return {-1,-1};
     
     }
 };
@@ -80,6 +93,7 @@ int main()
 {
     vector<int> nums = {1, 5, 8, 4, 3, 5};
     Solution s1;
-    s1.twoSum(nums, 11);
+    vector<int>ans = s1.twoSum(nums, 11);
+    cout<<ans[0]<<" "<<ans[1];
     return 0;
 }
