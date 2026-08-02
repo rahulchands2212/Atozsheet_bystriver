2b8e89138dc6c6171a410ad4e31211bbbce5154a
Rahul Chand <chandrahuls2212@gmail.com>
2026-08-02 09:57:34 +0530

    print max sub array

--- array/medium/kadanesalogo_withprintsubarray.cpp
+++ array/medium/kadanesalogo_withprintsubarray.cpp
@@ -1,29 +1,50 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int maxSubArray(vector<int>& nums) {
+    int maxSubArray(vector<int> &nums)
+    {
         int sum = 0;
         int maxi = INT_MIN;
         int n = nums.size();
 
-        for(int i=0;i<n;i++){
+        int start = 0;
+        int end = 0;
+        int tempstart = 0;
+        for (int i = 0; i < n; i++)
+        {
             sum = sum + nums[i];
-            cout<<maxi<<" ";
-            if(sum<0){
+
+            if (maxi < sum)
+            {
+                maxi = sum;
+                start = tempstart;  //when we find max then we confirmed start index
+                end = i;
+            }
+
+            if (sum < 0)
+            {
                 sum = 0;
+                tempstart = i + 1;  //assume start with that index
             }
         }
 
+        for (int i = start; i <= end; i++)
+        {
+            cout << nums[i] << " ";
+        }
+        cout << " = ";
+
         return maxi;
-        
     }
 };
 
-int main(){
-    vector<int>nums = {2, 3, 5, -2, 7, -4};
+int main()
+{
+    vector<int> nums = {-2, -3, 4, -1, -2, 1, 5, -3};
     Solution s1;
-   cout<<s1.maxSubArray(nums);
-   return 0;
+    cout << s1.maxSubArray(nums);
+    return 0;
 }
